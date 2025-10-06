1aba8a515287e9c8d6f48a383581bd0171af5b9b
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2025-10-06 18:23:49 +0530

    commit 1

---
+++ AsynchronousProductFetchDemo/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProductFetchDemo
+{
+    public  class Product
+    {
+        public string Name { get; set; }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ AsynchronousProductFetchDemo/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProductFetchDemo
+{
+    public class Review
+    {
+        public string Content { get; set; }
+
+        public Review(string content)
+        {
+            Content = content;
+        }
+    }
+}
